62185cf0730d27e88579715d4f83bd6cbf1e9182
RafatMeraz <razhossain201@gmail.com>
2023-11-16 23:38:18 +0600

    class and object

--- oop/class.dart
+++ oop/class.dart
@@ -1,17 +1,59 @@
 void main() {
-  int a = 34;
-  Person rafat = Person();
-  rafat.name = 'asfdsafads';
-  rafat.noOfHands = 34;
-  rafat.noOfLegs = 6;
+  int g = 3;
+  // object/instance creation
+  Student studentOne = Student('Rafi', 26, 'dsfasdf');
 
-  print(rafat.name);
-  print(rafat.noOfLegs);
-  print(Person());
+  print(studentOne.name);
+  print(studentOne.age);
+  print(studentOne.address);
+  studentOne.doing('abc');
+  studentOne.coding();
+
+  Student studentTwo = Student('Tanmoy', 23, 'Dhaka Mirpur');
+
+  print(studentTwo.name);
+  print(studentTwo.age);
+  print(studentTwo.address);
+  studentTwo.coding();
+  studentTwo.doing('home wrok');
+  int result = studentTwo.add(12, 13);
+  print(result);
+
+  Student studentThree = Student('Zehad', 21, 'Tangail');
+  print(studentThree.name);
+
+  print(Student.noOfObjects);
 }
 
-class Person {
-  String name = '';
-  int noOfHands = 0;
-  int noOfLegs = 0;
+// class
+class Student {
+  // properties/attributes
+  late String name;
+  late int age;
+  late String address;
+
+  static int noOfObjects = 0;
+
+  // construct the object of this class
+  // Constructor
+  Student(String name, int a, String address) {
+    print('Object is creating');
+    this.name = name;
+    age = a;
+    this.address = address;
+    noOfObjects += 1;
+  }
+
+  // method
+  void coding() {
+    print('$name is coding');
+  }
+
+  void doing(String doingWhat) {
+    print('$name is doing $doingWhat');
+  }
+
+  int add(int a, int b) {
+    return a + b;
+  }
 }
