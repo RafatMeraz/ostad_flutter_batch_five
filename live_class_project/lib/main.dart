53c81dbfe53c9513221b767b094cb6d269ace767
RafatMeraz <razhossain201@gmail.com>
2023-12-12 23:48:02 +0600

    basic hello world project added

---
+++ live_class_project/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo app',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen,
+      body: Center(
+        child: Text('Hello world'),
+      ),
+    );
+  }
+
+}
