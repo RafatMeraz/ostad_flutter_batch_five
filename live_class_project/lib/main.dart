e99ffd32e10afd230634b9198d42b4d3e0cb52a3
RafatMeraz <razhossain201@gmail.com>
2023-12-14 23:33:55 +0600

    - Text - Image - Center - AppBar

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -18,11 +18,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightGreen,
+      backgroundColor: Color(0xFF10C419),
+      appBar: AppBar(
+        leading: Icon(Icons.home, color: Colors.black54, size: 32,),
+        title: Text('Home', style: TextStyle(
+          color: Colors.black
+        ),),
+        backgroundColor: Colors.yellow,
+        elevation: 5,
+      ),
+      // body: Center(
+      //   child: Text(
+      //     'Hello ostad. We are going to complete the flutter skjds kjdkksj dksjd j',
+      //     textAlign: TextAlign.center,
+      //     maxLines: 3,
+      //     style: TextStyle(
+      //       color: Colors.white,
+      //       fontSize: 32,
+      //       fontWeight: FontWeight.bold,
+      //       // backgroundColor: Colors.grey,
+      //       decoration: TextDecoration.underline,
+      //       wordSpacing: 4,
+      //       letterSpacing: 5,
+      //       overflow: TextOverflow.ellipsis
+      //     ),
+      //   ),
+      // ),
       body: Center(
-        child: Text('Hello world'),
+        // child: Image.network(
+        //   'https://mdbootstrap.com/docs/standard/plugins/dummy/dummy.jpg',
+        // ),
+        child: Image.asset(
+          'images/uncle-bob.jpg',
+          height: 600,
+          width: 300,
+          fit: BoxFit.scaleDown,
+          // color: Colors.grey,
+          // repeat: ImageRepeat.repeatY,
+        ),
       ),
     );
   }
-
 }
