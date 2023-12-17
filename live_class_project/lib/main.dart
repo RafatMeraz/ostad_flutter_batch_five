5bd71379fdd3122aeb7c771dcadd73d2bd6f038f
RafatMeraz <razhossain201@gmail.com>
2023-12-17 23:23:31 +0600

    - Column - Row - Elevated Button - alertDialog - bottomSheet - RichText

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo app',
@@ -15,48 +16,126 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF10C419),
       appBar: AppBar(
-        leading: Icon(Icons.home, color: Colors.black54, size: 32,),
-        title: Text('Home', style: TextStyle(
-          color: Colors.black
-        ),),
-        backgroundColor: Colors.yellow,
+        title: Text('Home',),
         elevation: 5,
       ),
-      // body: Center(
-      //   child: Text(
-      //     'Hello ostad. We are going to complete the flutter skjds kjdkksj dksjd j',
-      //     textAlign: TextAlign.center,
-      //     maxLines: 3,
-      //     style: TextStyle(
-      //       color: Colors.white,
-      //       fontSize: 32,
-      //       fontWeight: FontWeight.bold,
-      //       // backgroundColor: Colors.grey,
-      //       decoration: TextDecoration.underline,
-      //       wordSpacing: 4,
-      //       letterSpacing: 5,
-      //       overflow: TextOverflow.ellipsis
-      //     ),
-      //   ),
+      // body: Column(
+      //   children: [
+      //     // Vertically
+      //     Text('Hello'),
+      //     Text('World'),
+      //   ],
       // ),
-      body: Center(
-        // child: Image.network(
-        //   'https://mdbootstrap.com/docs/standard/plugins/dummy/dummy.jpg',
-        // ),
-        child: Image.asset(
-          'images/uncle-bob.jpg',
-          height: 600,
-          width: 300,
-          fit: BoxFit.scaleDown,
-          // color: Colors.grey,
-          // repeat: ImageRepeat.repeatY,
-        ),
-      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          // Horizontally arrange
+          Text('Hello'),
+          Text('World'),
+          Text('World'),
+          Image.asset(
+            'images/uncle-bob.jpg',
+            width: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text('sdfd'),
+              Text('alsk fkfl skd'),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('sdfsdf'),
+              Text('sdfsdf'),
+              Text('sdfsdf'),
+              Text('sdfsdf'),
+              Text('sdfsdf'),
+            ],
+          ),
+          // Text('Hello from ostad'),
+          RichText(text: TextSpan(
+            text: 'Hello ',
+            style: TextStyle(
+                color: Colors.pink,
+              fontSize: 24
+              ),
+            children: [
+              TextSpan(
+                text: 'from',
+                style: TextStyle(
+                  color: Colors.black54
+                )
+              ),
+              TextSpan(
+                text: ' Ostad',
+                style: TextStyle(
+                  fontSize: 12
+                )
+              ),
+            ]
+          ),
+          ),
+          RichText(text: TextSpan(
+            text: 'Please accept the ',
+            style: TextStyle(
+                color: Colors.pink,
+              fontSize: 24
+              ),
+            children: [
+              TextSpan(
+                text: 'term and condition',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                )
+              ),
+              TextSpan(
+                text: ' for use the app.',
+              ),
+            ]
+          ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // showAboutDialog(context: context,);
+              // showDialog(context: context, builder: (context) {
+              //   return AlertDialog(
+              //     title: Text('Message'),
+              //     content: Text('Hello, welcome to app'),
+              //     actions: [
+              //       ElevatedButton(onPressed: () {}, child: Text('cancel'),),
+              //     ],
+              //   );
+              // });
+
+              showModalBottomSheet(
+                  isDismissible: false,
+                  backgroundColor: Colors.yellow,
+                  barrierColor: Colors.pink.shade300,
+                  context: context,
+                  builder: (_) {
+                    return Column(
+                      children: [
+                        Text('Message'),
+                        Text('Hello world'),
+                      ],
+                    );
+              });
+            },
+            child: Text('Tap here'),
+          ),
+        ],
+      ) ,
     );
   }
 }
