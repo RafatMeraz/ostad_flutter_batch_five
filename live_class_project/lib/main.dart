a3b6edcaaba0192d2e88adafe71e4d43e0d29a77
RafatMeraz <razhossain201@gmail.com>
2024-01-04 23:35:03 +0600

    - Flexible - Expanded - AspectRatio - SizedBox - FractionallySizedBox

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-/// Orientation Builder
-/// Layout Builder
-/// MediaQuery
-/// Wrap
-/// Const uses before widget
+/// Flexible
+/// Expanded
+/// AspectRatio
+/// SizedBox
+/// FractionallySizedBox
 
 void main() {
   runApp(const MyApp());
@@ -33,94 +33,64 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-
-    // print(MediaQuery.of(context).orientation);
-    // print(MediaQuery.of(context).size.height);
-    // print(MediaQuery.of(context).size.width);
-    // print(MediaQuery.of(context).size.aspectRatio);
-    // print(MediaQuery.of(context).size.flipped);
-    // print(MediaQuery.of(context).size.longestSide);
-    // print(MediaQuery.of(context).size.shortestSide);
-    // print(MediaQuery.of(context).displayFeatures);
-    // print(MediaQuery.of(context).devicePixelRatio);
-    //
-    // print(MediaQuery.sizeOf(context));
-    // print(MediaQuery.orientationOf(context));
-    // print(MediaQuery.devicePixelRatioOf(context));
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        backgroundColor: Colors.blue,
+        title: const Text('Home Screen'),
       ),
-     /* body: Scrollbar(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Scrollbar(
-                thickness: 3,
-                radius: Radius.circular(30),
-                interactive: true,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  // scrollDirection: Axis.vertical,
-                  child: Row(
-                    children: [
-                      Text('asdfsdfsdfsdfasdfsdf ksadflilsdkf lskd lfk'),
-                      Text('asdfsdfsdfsdfasdf'),
-                      Text('asdfsdfsdfsdfasdfsdf ksadflilsdkf lskd lfk'),
-                    ],
-                  ),
-                ),
-              ),
-              const Wrap(
-                alignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                spacing: 10,
-                children: [
-                  Text('asdfsdfsdfsdfasdfsdf ksad'),
-                  Text('saf'),
-                  Text('asdfsdfsdfsdfasdfsdf ksadflilsdkf lskd lfk'),
-                ],
-              ),
-
-              OrientationBuilder(
-                builder: (context, orientation) {
-                  print(orientation);
-                  if (orientation == Orientation.portrait) {
-                    return const Text('Screen looks good');
-                  } else if (orientation == Orientation.landscape){
-                    return const Text('Screen is too wider');
-                  } else {
-                    return const Text('Unknown');
-                  }
-                },
-              ),
-            ],
+      body: Column(
+        children: [
+        /*  Expanded(
+            flex: 2,
+            child: Container(
+              width: double.infinity,
+              color: Colors.purple,
+            ),
           ),
-        ),
-      ),*/
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          print(constraints);
-          if (constraints.maxWidth < 300) {
-            return Text('Tiny device');
-          } else if (constraints.maxWidth < 500) {
-            return Text('Normal device');
-          } else if (constraints.maxWidth < 700) {
-            return Text('Tablet device');
-          } else if (constraints.maxWidth < 1200) {
-            return Text('Laptop device');
-          } else {
-            return Text('Too large');
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          count++;
-          setState(() {});
-        },
-        child: const Icon(Icons.add),
+          Expanded(
+            flex: 3,
+            child: Container(
+              width: double.infinity,
+              color: Colors.grey,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              width: double.infinity,
+              color: Colors.red,
+            ),
+          )*/
+         /* SizedBox(
+            width: 100,
+            height: 100,
+            child: Text('safjsdjfskdjf ksdkf jkdj fdj AD JSKJ S  SDJ SKDJK SJD S JKJS DSKD JKASKDJSK K JKSDJ S '),
+          ),
+          SizedBox(
+            height: 500,
+            width: 2000,
+            child: FractionallySizedBox(
+              heightFactor: 0.9,
+              widthFactor: 0.7,
+              alignment: Alignment.center,
+              child: Text('sjf klsdjfjsdf jsdjf ksdjkf ls'),
+            ),
+          ),
+          SizedBox(
+            width: 100,
+            height: 100,
+            child: Text('sdfsdf'),
+          )*/
+          SizedBox(
+            width: 100,
+            child: AspectRatio(
+              aspectRatio: 2/2,
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
