11044f15453f62ec0bb72d84b30b0b603784e5d0
RafatMeraz <razhossain201@gmail.com>
2024-01-07 23:12:28 +0600

    - Device Preview - BuildContext - Stack - Positioned - Align

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-/// Flexible
-/// Expanded
-/// AspectRatio
-/// SizedBox
-/// FractionallySizedBox
+/// Device Preview
+/// BuildContext
+/// Package intro (Package vs. Plugin)
+/// Stack
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) {
+      return const MyApp();
+    }
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -38,59 +43,35 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.blue,
         title: const Text('Home Screen'),
       ),
-      body: Column(
-        children: [
-        /*  Expanded(
-            flex: 2,
-            child: Container(
-              width: double.infinity,
-              color: Colors.purple,
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
             ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              width: double.infinity,
-              color: Colors.grey,
-            ),
-          ),
-          Expanded(
-            flex: 5,
-            child: Container(
-              width: double.infinity,
-              color: Colors.red,
-            ),
-          )*/
-         /* SizedBox(
-            width: 100,
-            height: 100,
-            child: Text('safjsdjfskdjf ksdkf jkdj fdj AD JSKJ S  SDJ SKDJK SJD S JKJS DSKD JKASKDJSK K JKSDJ S '),
-          ),
-          SizedBox(
-            height: 500,
-            width: 2000,
-            child: FractionallySizedBox(
-              heightFactor: 0.9,
-              widthFactor: 0.7,
-              alignment: Alignment.center,
-              child: Text('sjf klsdjfjsdf jsdjf ksdjkf ls'),
-            ),
-          ),
-          SizedBox(
-            width: 100,
-            height: 100,
-            child: Text('sdfsdf'),
-          )*/
-          SizedBox(
-            width: 100,
-            child: AspectRatio(
-              aspectRatio: 2/2,
-              child: Container(
-                color: Colors.red,
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.purple,
+                ),
               ),
             ),
-          )
-        ],
+            Positioned(
+              bottom: 5,
+              right: 5,
+              child: Container(
+                width: 30,
+                height: 30,
+                color: Colors.amber,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
