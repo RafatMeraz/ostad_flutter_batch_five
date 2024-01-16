69884e75bf6d8a30304ed29411fe0a86c463b8aa
RafatMeraz <razhossain201@gmail.com>
2024-01-16 23:38:08 +0600

    - Bottom Navigation Bar - TabBar & TabBarView

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,18 +1,7 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-/// Device Preview
-/// BuildContext
-/// Package intro (Package vs. Plugin)
-/// Stack
-
 void main() {
-  runApp(DevicePreview(
-    enabled: true,
-    builder: (context) {
-      return const MyApp();
-    }
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,8 +9,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: Colors.purple
+      ),
+      home: const SecondScreen(),
     );
   }
 }
@@ -34,45 +26,113 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int count = 0;
+  int _selectedIndex = 0;
+
+  List<Widget> screens = [
+    const Text('Home'),
+    const Text('Profile'),
+    const Text('Search'),
+    const Text('Settings'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Home Screen'),
+        backgroundColor: Colors.lightGreen,
+        title: const Text('Home'),
       ),
-      body: Center(
-        child: Stack(
+      body: Center(child: screens[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 5,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.lightGreen,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedFontSize: 14,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(
+          fontSize: 18,
+          color: Colors.black,
+          fontWeight: FontWeight.bold
+        ),
+        unselectedLabelStyle: const TextStyle(
+          // TODO: eexplore it
+        ),
+        // fixedColor: Colors.grey,
+        onTap: (index) {
+          print(index);
+          _selectedIndex = index;
+          setState(() {});
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key});
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> /*with SingleTickerProviderStateMixin*/ {
+  // late final TabController _tabController = TabController(length: 3, vsync: this);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      animationDuration: const Duration(milliseconds: 300),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Facebook'),
+          bottom: TabBar(
+            indicator: UnderlineTabIndicator(
+              borderSide: const BorderSide(width: 5, color: Colors.white),
+              borderRadius: BorderRadius.circular(2),
+              insets: const EdgeInsets.symmetric(horizontal: 80)
+            ),
+            unselectedLabelColor: Colors.grey,
+            labelColor: Colors.white,
+            indicatorWeight: 3,
+            tabs: const [
+              Tab(
+                icon: Icon(Icons.feed),
+                text: 'Feed',
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+                text: 'Profile',
+              ),
+              Tab(
+                icon: Icon(Icons.search),
+                text: 'Search',
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
           children: [
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.green,
-            ),
-            Positioned.fill(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  color: Colors.purple,
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 5,
-              right: 5,
-              child: Container(
-                width: 30,
-                height: 30,
-                color: Colors.amber,
-              ),
-            ),
+            HomeScreen(),
+            Center(child: Text('Profile')),
+            Center(child: Text('Search')),
           ],
         ),
       ),
     );
   }
 }
+
+
